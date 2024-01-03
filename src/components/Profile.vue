6224651fdd750cc3a3b8a1c6a75457c6e71fdc90
Rachad-Alabi-ADEKAMBI <adekambirachad@gmail.com>
2024-01-03 12:27:20 +0100

    profile page

---
+++ src/components/Profile.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="profile">
+        <div class="profile__pic">
+            <img src="" alt="full stack developer">
+
+        </div>
+
+        <div class="profile__details">
+            <div class="h2">
+                General informations
+            </div>
+
+            <div class="profile__details__list">
+                <p>
+                    <ul>
+                        <li>
+                            <i></i>
+                            <span>Name: </span>
+                            Rachad Alabi ADEKAMBI
+                        </li>
+
+                        <li>
+                            <i></i>
+                            <span>Location: </span>
+                            Benin republic
+                        </li>
+                    </ul>
+                </p>
+            </div>
+        </div>
+    </div>
+
+</template>
+
+<script>
+export default {
+  name: 'Profile',
+  props: {
+    msg: String
+  }
+}
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,17 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="main">
+      <Profile/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Profile from '@/components/Profile.vue'
 
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
+name: 'HomeView',
+components: {
+  Profile
+}
 }
 </script>
