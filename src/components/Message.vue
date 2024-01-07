6d6a987ed218b54f8f54aa18c0cddd6af5ff89c4
Rachad-Alabi-ADEKAMBI <adekambirachad@gmail.com>
2024-01-07 17:29:16 +0100

    update

---
+++ src/components/Message.vue
@@ -0,0 +1,21 @@
+<template>
+         <div class="message">
+           <div class="message__text">
+             <p>
+               Available for freelance work
+             </p>
+           </div>
+            <div class="message__btn">
+             <i class="fas fa-download"></i>
+           </div>
+         </div>
+ </template>
+
+ <script>
+ export default {
+   name: 'Message',
+   props: {
+     msg: String
+   }
+ }
+ </script>
