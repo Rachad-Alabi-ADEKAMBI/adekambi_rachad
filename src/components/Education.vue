26ad6b48f8aa753641ec80d9967dcf7189743128
Rachad-Alabi-ADEKAMBI <adekambirachad@gmail.com>
2024-01-09 12:54:08 +0100

    update

--- src/components/Education.vue
+++ src/components/Education.vue
@@ -1,19 +1,39 @@
 <template>
-    <div class="education">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus illum ullam amet obcaecati libero a nihil fugiat vero explicabo ad? Aut maxime molestiae corporis illo ratione, sapiente excepturi expedita veritatis.
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Necessitatibus nihil, neque, vitae dolore ea cum soluta veniam perspiciatis fugit laboriosam explicabo maxime mollitia quibusdam in consectetur, tempora fuga! Nam, voluptate.
-      </div>
+  <div class="infos">
+
+       <div class="message">
+         <div class="message__text">
+           <p>
+             Available for freelance work
+           </p>
+         </div>
+          <div class="message__btn">
+           <i class="fas fa-download"></i>
+         </div>
+       </div>
+
+       <h1>
+         My education
+       </h1>
+
+       <div class="education">
+          <div class="education__date">
+              2020
+          </div>
+
+          <div class="education__details">
+              <img src="img/open_classrooms.jpg" alt="openclassrooms student">
+              <p>B.Sc. in Computer System and Software </p>
+          </div>
+       </div>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'Education',
-  props: {
-    msg: String
-
-
-
-
-  }
+ name: 'Education',
+ props: {
+   msg: String
+ }
 }
 </script>
--- src/components/Experience.vue
+++ src/components/Experience.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="infos about">
+  <div class="infos">
 
        <div class="message">
          <div class="message__text">
@@ -16,10 +16,52 @@
          My experience
        </h1>
 
-       <p>
-       exp
-       </p>
+       <div class="experience">
+            <div class="experience__date">
+                01/2022 - nowaday
+            </div>
 
+            <div class="experience__details">
+                <span>
+                    Freelance full-stack developer
+                </span> <br>
+
+                <strong>
+                    ComeUp, Upwork
+                </strong>
+
+                <p>
+                  I provide web solutions and meet the needs of customers via technologies like wordpress, prestashop, nodejs. I also provided other applications like:
+                  a real time dashboard for statistics analysis and back office management of the android application teepzy (VueJs, wordpress);
+                  Development of the plugin that allows to automate hosting services and domain name setup (IaaS) (wordpress, shell);
+                  Realization of wordpress website;
+                  Customization of theme at the request of the customer;
+                  Creation and Integration of wordpress theme;
+                  Customization of wordpress dashboard at the request of the customer;
+                </p>
+            </div>
+       </div> 
+
+
+       <div class="experience">
+            <div class="experience__date">
+                01/2020 - 01/2022
+            </div>
+
+            <div class="experience__details">
+                <span>
+                    Junior developer
+                </span> <br>
+
+                <strong>
+                    Oroubouros
+                </strong>
+
+                <p>
+                  I provide web solutions and meet the needs of customers via technologies like wordpress, prestashop, nodejs
+                </p>
+            </div>
+       </div>
 
   </div>
 </template>
