85ec8af59907846de81b553042b3b187022a2b19
Rachad-Alabi-ADEKAMBI <adekambirachad@gmail.com>
2024-01-11 13:55:56 +0100

    update

--- src/components/Portfolio.vue
+++ src/components/Portfolio.vue
@@ -1,16 +1,43 @@
 <template>
-   <div class="portfolio">
-    <ul>
-        <li>portfolio</li>
-    </ul>
-   </div>
+  <div class="infos">
+
+       <div class="message">
+         <div class="message__text">
+           <p>
+             Available for freelance work
+           </p>
+         </div>
+          <div class="message__btn">
+           <i class="fas fa-download"></i>
+         </div>
+       </div>
+
+       <h1>
+          My portfolio
+        </h1>
+
+       <div class="portfolio">
+          <div class="portfolio__menu">
+              <a href="">All</a>
+              <a href="">Websites</a>
+              <a href="">Api projects and wordpress plugins</a>
+          </div>
+
+          <div class="portfolio__items">
+            <div class="item">
+              <img src="" alt="">
+            </div>
+          </div>
+       </div>
+
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'Portfolio',
-  props: {
-    msg: String
-  }
+ name: 'Portfolio',
+ props: {
+   msg: String
+ }
 }
 </script>
