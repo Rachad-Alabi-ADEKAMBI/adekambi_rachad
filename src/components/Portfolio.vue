154826547d12bda74e98ddb0dfed0380154839f5
Rachad-Alabi-ADEKAMBI <adekambirachad@gmail.com>
2024-01-12 11:40:40 +0100

    update

--- src/components/Portfolio.vue
+++ src/components/Portfolio.vue
@@ -1,43 +1,71 @@
 <template>
   <div class="infos">
+    <div class="message">
+      <div class="message__text">
+        <p>Available for freelance work</p>
+      </div>
+      <div class="message__btn">
+        <i class="fas fa-download"></i>
+      </div>
+    </div>
 
-       <div class="message">
-         <div class="message__text">
-           <p>
-             Available for freelance work
-           </p>
-         </div>
-          <div class="message__btn">
-           <i class="fas fa-download"></i>
-         </div>
-       </div>
+    <h1>My portfolio</h1>
 
-       <h1>
-          My portfolio
-        </h1>
+    <div class="portfolio">
+      <div class="portfolio__menu">
+        <span @click="displayAll()">All</span>
+        <span @click="displayWebsites()">Websites</span>
+        <span @click="displayPlugins()">API projects and WordPress plugins</span>
+      </div>
 
-       <div class="portfolio">
-          <div class="portfolio__menu">
-              <a href="">All</a>
-              <a href="">Websites</a>
-              <a href="">Api projects and wordpress plugins</a>
-          </div>
-
-          <div class="portfolio__items">
-            <div class="item">
-              <img src="" alt="">
-            </div>
-          </div>
-       </div>
-
+      <div class="portfolio__items" v-if="showAll">
+        <div class="item" v-for="detail in all" :key="detail.id">
+          <img :src="getImgUrl(detail.pic1)" alt="">
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
- name: 'Portfolio',
- props: {
-   msg: String
- }
-}
+  name: 'Portfolio',
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+      showAll: true,
+      showWebsites: false,
+      showPlugins: false,
+      all: [
+        { id: 1, name: 'nbroker', pic1: '/sites/nbroker/nb1.jpg', pic2: '/sites/nbroker/nb2.jpg' }
+      ]
+    };
+  },
+  mounted() {
+    this.displayAll();
+  },
+  computed: {},
+  methods: {
+    displayAll() {
+      this.showAll = true;
+      this.showWebsites = false;
+      this.showPlugins = false;
+    },
+    displayWebsites() {
+      this.showAll = false;
+      this.showWebsites = true;
+      this.showPlugins = false;
+    },
+    displayPlugins() {
+      this.showAll = false;
+      this.showWebsites = false;
+      this.showPlugins = true;
+    },
+    getImgUrl(pic) {
+    //  return require(`@/public/img/${pic}`);
+    }
+  }
+};
 </script>
