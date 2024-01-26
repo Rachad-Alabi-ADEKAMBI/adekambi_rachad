37f4bc9a951a17518120db2485bf09a557bee896
Rachad-Alabi-ADEKAMBI <adekambirachad@gmail.com>
2024-01-26 18:58:50 +0100

    update

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -34,8 +34,10 @@
             <div class="skill">
                 <img src="/public/img/logos/langages/php.png" alt="">
                 <img src="/public/img/logos/langages/mysql.png" alt="">
-                <img src="/public/img/logos/langages/html.png" alt="">
-            </div>
+                <img src="/public/img/logos/langages/htmlr.png" alt="">
+
+                <img src="/public/img/logos/langages/htmlr.png" alt="">
+              </div>
           </div>
        </div>
 
