7ec612895b8220e7868542e8b59958d55f5efeae
Rachad-Alabi-ADEKAMBI <adekambirachad@gmail.com>
2024-01-28 14:19:23 +0100

    update

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -23,7 +23,7 @@
           </h1>
 
           <p>
-            I have 4 years of experience as a back-end developer,
+            I have 4 years of experience as a full stack developer,
             during these 2 years I was able to acquire thanks to my ability to quickly learn more tips to perfect my work and discover a lot of new technologies.
           </p>
 
