ef1af7fbece6d8651d21994cd02b6a0edcb96ca6
Rachad-Alabi-ADEKAMBI <adekambirachad@gmail.com>
2024-02-20 13:11:56 +0100

    update

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -1,64 +1,91 @@
 <template>
   <div class="infos about">
+    <div class="message">
+      <div class="message__text">
+        <p>
+          Available for freelance work
+        </p>
+      </div>
+      <div class="message__btn">
+        <a href='mailto:adekambirachad@gmail.com'>
+          <i class="fas fa-download"></i>
+        </a>
+      </div>
+    </div>
 
-       <div class="message">
-         <div class="message__text">
-           <p>
-             Available for freelance work
-           </p>
-         </div>
-         <div class="message__btn">
-              <a href='mailto:adekambirachad@gmail.com'>
-                <i class="fas fa-download"></i>
-              </a>
-          </div>
-       </div>
+    <div class="skills">
+      <span>
+        What I am good at
+      </span>
 
-       <div class="skills">
-            <span>
-            What I am good at
+      <h1>
+        My skills
+      </h1>
 
-          </span>
+      <p>
+        I have 4 years of experience as a full stack developer,
+        during these 2 years I was able to acquire thanks to my ability to quickly learn more tips to perfect my work and discover a lot of new technologies.
+      </p>
 
-          <h1>
-            My skills
-          </h1>
+      <div class="skills__list">
+        <div class="skill">
+          <img src="img/logos/langages/js_logo.png" alt="">
 
-          <p>
-            I have 4 years of experience as a full stack developer,
-            during these 2 years I was able to acquire thanks to my ability to quickly learn more tips to perfect my work and discover a lot of new technologies.
-          </p>
-
-          <div class="skills__list">
-
-            <div class="skill">
-              <img src="img/logos/langages/php.png" alt="">
-              <img src="img/logos/langages/mysql.png" alt="">
-              <img src="img/logos/langages/python.png" alt="">
-
-              <img src="img/logos/langages/js_logo.png" alt="">
-
-                <img src="img/logos/frameworks/laravel.png" alt="">
-                 <img src="img/logos/frameworks/vue.png" alt="">
-                 <img src="img/logos/frameworks/flask.png" alt="">
-                 <img src="img/logos/frameworks/bootstrap.png" alt="">
-
-                 <img src="img/logos/tools/git.png" alt="">
-                 <img src="img/logos/os/linux.png" alt="">
-
-             </div>
-          </div>
-       </div>
+          <img src="img/logos/langages/js_logo.png" alt="">
 
 
+        </div> <br>
+
+        <h3>
+          Langages
+        </h3>
+
+        <div class="skill">
+          <img src="img/logos/langages/php.png" alt="">
+          <img src="img/logos/langages/mysql.png" alt="">
+          <img src="img/logos/langages/python.png" alt="">
+          <img src="img/logos/langages/js_logo.png" alt="">
+        </div> <br>
+
+        <h3>
+          Frameworks & Libraries
+        </h3>
+
+        <div class="skill">
+          <img src="img/logos/frameworks/laravel.png" alt="">
+          <img src="img/logos/frameworks/vue.png" alt="">
+          <img src="img/logos/frameworks/flask.png" alt="">
+          <img src="img/logos/frameworks/bootstrap.png" alt="">
+        </div>
+      </div> <br>
+
+      <h3>
+        Tools
+      </h3>
+
+      <div class="skill">
+        <img src="img/logos/tools/git.png" alt="">
+        <img src="img/logos/tools/git.png" alt="">
+      </div>  <br>
+
+      <h3>
+        Operating systems
+      </h3>
+
+      <div class="skill">
+        <img src="img/logos/os/linux.png" alt="">
+        <img src="" alt="">
+      </div>
+
+    </div>
   </div>
 </template>
 
 <script>
 export default {
- name: 'Skills',
- props: {
-   msg: String
- }
+  name: 'Skills',
+  props: {
+    msg: String
+  }
 }
 </script>
