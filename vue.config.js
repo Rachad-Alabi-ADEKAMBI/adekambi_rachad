2a372f341270d6168a80ba1ad947982bdfa8db5f
Rachad-Alabi-ADEKAMBI <adekambirachad@gmail.com>
2024-02-26 10:12:21 +0100

    update

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,11 @@
-const { defineConfig } = require('@vue/cli-service')
+// vue.config.js
+const { defineConfig } = require('@vue/cli-service');
+
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  transpileDependencies: true,
+  publicPath: "/portfolio/",
+  chainWebpack: (config) => {
+    // Additional webpack configurations if needed
+  },
+  // Other Vue CLI options...
+});
