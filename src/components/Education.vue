07ae5494f6b22f4e7d6c8d603c82682293d22124
Rachad-Alabi-ADEKAMBI <adekambirachad@gmail.com>
2024-03-17 20:37:17 +0100

    update

--- src/components/Education.vue
+++ src/components/Education.vue
@@ -126,7 +126,7 @@
             <p>
               <span>Open Classrooms</span> <br>
               <strong>SEO</strong> <br>
-              <a href="img/seo.pdf">View certificate</a>
+              <a href="img/certifications/api.pdf">View certificate</a>
             </p>
           </div>
 
@@ -142,7 +142,7 @@
             <p>
               <span>Open classroom</span> <br>
               <strong>Algorithmic</strong> <br>
-              <a href="algorithmes.pdf">View certificate</a>
+              <a href="img/certifications/api/algorithmes.pdf">View certificate</a>
             </p>
           </div>
 
